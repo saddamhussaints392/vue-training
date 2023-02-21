26cfc0b0388026809dab45e21b24054d534c1f3b
saddamhussaints392 <saddam.ts@hyconsoft.in>
2023-02-21 16:03:28 +0530

    signup with email and google completed

--- src/main.js
+++ src/main.js
@@ -8,6 +8,22 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/main.css'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCKC1He9I_xZmNs61VALSI6LLXNnLrlysg",
+  authDomain: "food-app-de1c6.firebaseapp.com",
+  projectId: "food-app-de1c6",
+  storageBucket: "food-app-de1c6.appspot.com",
+  messagingSenderId: "18104406601",
+  appId: "1:18104406601:web:c395ac67cd859f4f6ea598"
+};
+
+initializeApp(firebaseConfig);
 
 configureCompat({
     MODE: 2,
--- src/pages/SignUp.vue
+++ src/pages/SignUp.vue
@@ -15,15 +15,15 @@
         <div class="signup_main">
           <span class="d-block signup_text">Sign Up</span>
           <div class="position-relative name_field mt-4">
-            <b-form-input class="rounded-0" placeholder="Name"></b-form-input>
+            <b-form-input class="rounded-0" placeholder="Name" v-model="name"></b-form-input>
             <b-icon-person font-scale="1.5" class="position-absolute person_icon"></b-icon-person>
           </div>
           <div class="position-relative email_field mt-3">
-            <b-form-input class="rounded-0" placeholder="Email"></b-form-input>
+            <b-form-input class="rounded-0" placeholder="Email" v-model="email"></b-form-input>
             <b-icon-wallet font-scale="1.3" class="position-absolute person_icon"></b-icon-wallet>
           </div>
           <div class="position-relative password_field mt-3">
-            <b-form-input class="rounded-0" placeholder="Password"></b-form-input>
+            <b-form-input class="rounded-0" placeholder="Password" v-model="password"></b-form-input>
             <b-icon-bag-x font-scale="1.3" class="position-absolute person_icon"></b-icon-bag-x>
           </div>
           <div class="mt-3">
@@ -37,7 +37,7 @@
             >Remember me ?</b-form-checkbox>
           </div>
           <div class="mt-4">
-            <button class="btn_signup w-100">Sign Up</button>
+            <button class="btn_signup w-100" @click="signUp">Sign Up</button>
           </div>
           <span class="d-block w-100 mt-3 forget_password_text">Forget password ?</span>
           <div class="d-flex align-items-center mt-4">
@@ -45,15 +45,19 @@
             <span class="or_text">OR</span>
             <hr class="flex-grow-1" />
           </div>
-          <div class="d-flex w-100 btn_google_signup mt-3 align-items-center justify-content-between">
-            <img src="../assets/images/Google.png" alt="" class="d-block pl-3">
-          <span class="d-block">Sign up with Google</span>
-          <span></span>
+          <div
+            class="d-flex w-100 btn_google_signup mt-3 align-items-center justify-content-between"
+          >
+            <img src="../assets/images/Google.png" alt class="d-block pl-3" />
+            <span class="d-block" @click="signUpWithGoogle">Sign up with Google</span>
+            <span></span>
           </div>
-          <div class="d-flex w-100 btn_apple_signup mt-3 align-items-center justify-content-between">
-            <img src="../assets/images/Apple.png" alt="" class="d-block pl-3">
-          <span class="d-block">Sign up with Apple</span>
-          <span></span>
+          <div
+            class="d-flex w-100 btn_apple_signup mt-3 align-items-center justify-content-between"
+          >
+            <img src="../assets/images/Apple.png" alt class="d-block pl-3" />
+            <span class="d-block" @click="signUpWithApple">Sign up with Apple</span>
+            <span></span>
           </div>
         </div>
       </b-col>
@@ -69,9 +73,84 @@
 import FoodAppBanner from "../components/FoodAppBanner.vue";
 import Footer from "../components/Footer.vue";
 import Navbar from "../components/Navbar.vue";
-
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  OAuthProvider
+} from "firebase/auth";
 export default {
-  components: { Navbar, FoodAppBanner, Footer }
+  components: { Navbar, FoodAppBanner, Footer },
+  data() {
+    return {
+      name: "",
+      password: "",
+      status: "",
+      email: ""
+    };
+  },
+  methods: {
+    signUp() {
+      console.log(this.name, this.password, this.email, this.status);
+      createUserWithEmailAndPassword(
+        getAuth(),
+        this.email,
+        this.password,
+        this.name,
+        this.status
+      )
+        .then(data => {
+          console.log("registered successfully");
+          // navigate the user after signUp successfull
+          // example  route.push("/home")
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    signUpWithGoogle() {
+      const provider = new GoogleAuthProvider();
+      signInWithPopup(getAuth(), provider)
+        .then(data => {
+          console.log(data.user);
+          console.log("registered successfully using Google Account");
+          // navigate the user after signUp successfull
+          // example  route.push("/home")
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    signUpWithApple() {
+      const provider = new OAuthProvider("apple.com");
+      signInWithPopup(getAuth(), provider)
+        .then(result => {
+          // The signed-in user info.
+          const user = result.user;
+          console.log(user);
+          console.log("registered successfully using Apple Account");
+          // Apple credential
+          const credential = OAuthProvider.credentialFromResult(result);
+          const accessToken = credential.accessToken;
+          const idToken = credential.idToken;
+
+          // IdP data available using getAdditionalUserInfo(result)
+          // ...
+        })
+        .catch(error => {
+          // Handle Errors here.
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // The email of the user's account used.
+          const email = error.customData.email;
+          // The credential that was used.
+          const credential = OAuthProvider.credentialFromError(error);
+
+          // ...
+        });
+    }
+  }
 };
 </script>
 <style scoped>
@@ -97,7 +176,9 @@ input {
   padding: 20px 45px;
   border: 1px solid #e0e0e0 !important;
 }
-.btn_signup,.btn_google_signup,.btn_apple_signup {
+.btn_signup,
+.btn_google_signup,
+.btn_apple_signup {
   background: #ff9f0d;
   padding-top: 10px;
   padding-bottom: 10px;
@@ -106,10 +187,11 @@ input {
   font-size: 16px;
   cursor: pointer;
 }
-.btn_google_signup, .btn_apple_signup {
+.btn_google_signup,
+.btn_apple_signup {
   color: #4f4f4f;
   background-color: #ffffff;
-  border: 1px solid #E0E0E0;
+  border: 1px solid #e0e0e0;
 }
 .forget_password_text {
   font-family: "Helvetica";
@@ -129,7 +211,7 @@ input {
 
 @media screen and (max-width: 480px) {
   .signup_section {
-  padding: 62px 15px;
-}
+    padding: 62px 15px;
+  }
 }
 </style>
